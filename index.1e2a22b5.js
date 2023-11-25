ebb6e3288f28123f6e52057ee10555465555872e
Monika-Lis <144853392+Monika-Lis@users.noreply.github.com>
2023-11-25 22:27:55 +0000

    Deploying to gh-pages from @ Monika-Lis/chocolate@7121ed8adf18a12857bdf12660607a2934254fb3 🚀

---
+++ index.1e2a22b5.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[review-modal-open]"),closeModalBtn:document.querySelector("[review-modal-close]"),modal:document.querySelector("[review-modal]")};function o(){e.modal.classList.toggle("review-is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.1e2a22b5.js.map
