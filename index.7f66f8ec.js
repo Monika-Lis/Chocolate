a67e30ad1e0e7336c1c52ffec018843a7fea0edf
Monika-Lis <144853392+Monika-Lis@users.noreply.github.com>
2023-11-25 21:47:25 +0000

    Deploying to gh-pages from @ Monika-Lis/chocolate@98f65b7407eb9339637c1dd7de78f4fb9985b337 🚀

---
+++ index.7f66f8ec.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[buy-modal-open]"),closeModalBtn:document.querySelector("[buy-modal-close]"),modal:document.querySelector("[buy-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.7f66f8ec.js.map
