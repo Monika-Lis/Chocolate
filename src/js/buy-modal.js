2c403e24aeda59323f9e7f5e7c68ed6ce9f16c56
Monika-Lis <lis.monikak@gmail.com>
2023-11-26 18:05:43 +0000

    updates

--- src/js/buy-modal.js
+++ src/js/buy-modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[buy-modal-open]"),
-    closeModalBtn: document.querySelector("[buy-modal-close]"),
-    modal: document.querySelector("[buy-modal]"),
+    openModalBtn: document.querySelector('[buy-modal-open]'),
+    closeModalBtn: document.querySelector('[buy-modal-close]'),
+    modal: document.querySelector('[buy-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('buy-is-hidden');
   }
 })();
